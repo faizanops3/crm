95608e104571c76989e1f3320f13d4bee68a664f
faizanops3 <faizanops@alazka.ai>
2025-04-27 03:44:57 +0530

    base and root url updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,10 +37,6 @@ import VoucherPage from "./Pages/Vouchers/VoucherPage";
 import Home from "./Client Panel/Home";
 import TranscriptPage from "./Pages/Transcript/TranscriptPage";
 
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 const App = () => {
   ///////////////////////////////////// VARIABLES ////////////////////////////////////////
   const { loggedUser } = useSelector((state) => state.user);
